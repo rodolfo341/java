905277b24db48b10f86635fa25b11809f55ea6b7
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-11-26 18:59:51 -0300

    Dia 2 de 100 de numero a ascci

--- discoDuroRoer/src/discoDuroroer_7/ej_7.java
+++ discoDuroRoer/src/discoDuroroer_7/ej_7.java
@@ -1,17 +1,25 @@
 package discoDuroroer_7;
 
+import java.util.Scanner;
+
 /**
  * Rodolfo aravena
  */
 
 /**
- * 
+ * Lee un número por teclado y muestra por consola, el carácter al que pertenece en la tabla ASCII. 
+ * Por ejemplo: si introduzco un 97, me muestre una a.
  */
 public class ej_7 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("ingrese un numero entre 0 y 255");
+		char caracter = (char)sc.nextInt();
+		System.out.println(caracter);
+		
 	}
 
 }
