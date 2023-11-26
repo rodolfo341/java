badaa7d3773559caba489f3286a0eca0f3ef341b
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-11-26 13:19:43 -0300

    Dia 2 de 100 concatenar

--- discoDuroRoer/src/discoDuroroer_3/ej_3.java
+++ discoDuroRoer/src/discoDuroroer_3/ej_3.java
@@ -1,10 +1,22 @@
 package discoDuroroer_3;
 
+/**
+ * Rodolfo aravena
+ */
+
+/**
+ * Declara un String que contenga tu nombre, 
+ * después muestra un mensaje de bienvenida por consola. 
+ * Por ejemplo: si introduzco «Fernando», me aparezca «Bienvenido Fernando».
+ */
 public class ej_3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		String nombre = "Rodolfo";
+		System.out.println("Bienvenido ".concat(nombre));
+		
 	}
 
 }
