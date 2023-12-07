e73f65096ab91cb0eb702f34e7548530e2799c78
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-12-06 22:32:04 -0300

    Dia 2 de 100 for

--- discoDuroRoer/src/discoDuroRoer_11/ej_11.java
+++ discoDuroRoer/src/discoDuroRoer_11/ej_11.java
@@ -5,6 +5,11 @@ public class ej_11 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		for (int i = 1; i <= 100; i++) {
+			System.out.println(i);
+		}
+		
+		
 	}
 
 }
