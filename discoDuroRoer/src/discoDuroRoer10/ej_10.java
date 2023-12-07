5c8af28e0b947aff770df226185882c97025b1d8
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-12-06 22:21:29 -0300

    Dia 2 de 100 while

--- discoDuroRoer/src/discoDuroRoer10/ej_10.java
+++ discoDuroRoer/src/discoDuroRoer10/ej_10.java
@@ -14,7 +14,12 @@ public class ej_10 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		int i = 1;
 		
+		while ( i <= 100 ) {
+			System.out.println(i);
+			i++;
+		}
 		
 	}
 }
