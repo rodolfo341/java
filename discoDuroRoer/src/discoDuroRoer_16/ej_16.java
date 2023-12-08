c7e505c508e40aaecc733bf56ed2c42a8ebecf97
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-12-08 19:14:22 -0300

    Dia 3 de 100 pasword

---
+++ discoDuroRoer/src/discoDuroRoer_16/ej_16.java
@@ -0,0 +1,20 @@
+package discoDuroRoer_16;
+/**
+ * Rodolfo Aravena
+ */
+
+/**
+ * Escribe una aplicación con un String que contenga una contraseña cualquiera. 
+ * Después se te pedirá que introduzcas la contraseña, con 3 intentos. 
+ * Cuando aciertes ya no pedirá mas la contraseña y mostrara un mensaje diciendo «Enhorabuena». 
+ * Piensa bien en la condición de salida (3 intentos y si acierta sale, aunque le queden intentos).
+ */
+
+public class ej_16 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
