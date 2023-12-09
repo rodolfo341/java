53067c3f1e256c57e1bb35c3052dec42aaaf904b
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-12-08 22:10:01 -0300

    Dia 3 de 100 pasword

--- discoDuroRoer/src/discoDuroRoer_16/ej_16.java
+++ discoDuroRoer/src/discoDuroRoer_16/ej_16.java
@@ -1,4 +1,7 @@
 package discoDuroRoer_16;
+
+import java.util.Scanner;
+
 /**
  * Rodolfo Aravena
  */
@@ -13,8 +16,28 @@ package discoDuroRoer_16;
 public class ej_16 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		// variables
+		char sino;
+		String respuesta;
+				
+		//Escribe una aplicación con un String que contenga una contraseña cualquiera.  
+		String pas = "pp";
+		
+		// cuerpo
+		// Después se te pedirá que introduzcas la contraseña, con 3 intentos.
+		for(int i = 1; i<= 3;i++) {
+			System.out.println("introduzca contraseña, intento " + i + " : ");
+			respuesta = sc.nextLine();
+			// Cuando aciertes ya no pedirá mas la contraseña y 
+			// mostrara un mensaje diciendo «Enhorabuena». 
+			if ( respuesta.equalsIgnoreCase(pas)) {
+				System.out.println("Ehnorabuena");
+				break;
+			}
+		} 
+		// Piensa bien en la condición de salida (3 intentos y si acierta sale, aunque le queden intentos).
+		
 	}
 
 }
