ac6ba59a69b941d433106856aa329f25b8dc2d49
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-12-09 23:43:33 -0300

    Dia 3 de 100 09/12/23 numero primo

---
+++ discoDuroRoer/src/discoDuroRoer_21/ej_21.java
@@ -0,0 +1,40 @@
+package discoDuroRoer_21;
+
+import java.util.Scanner;
+
+/**
+ * Rodolfo Aravena
+ * 
+ * Muestra los números primos entre 1 y 100
+ * 
+ */
+
+public class ej_21 {
+	
+	public static void main(String[] args) {
+		
+		for( int num = 2 ; num<=100 ; num++ ) {
+			boolean isPrimo = true;
+							
+			for(int i = 2 ; i<= Math.sqrt(num) ; i++) {
+				if( num % i == 0 ) {
+					isPrimo = false;
+					break;
+				}
+			}
+			
+			if(isPrimo) System.out.println(num);
+			
+		}			
+				
+	}
+}
+/*
+Scanner sc = new Scanner(System.in);
+char res;
+
+do {
+	
+	System.out.print("¿Desea continuar? (S/N): "); res = sc.next().charAt(0);
+} while (Character.toUpperCase(res) == 'S');
+*/
