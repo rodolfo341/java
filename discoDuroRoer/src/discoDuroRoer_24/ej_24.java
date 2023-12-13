e3fce9fcbaf9cb7fdf0206ae523c97c39ce88cc7
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-12-13 20:44:49 -0300

    Dia 5 de 100 12/12/23 escci de cada letra del string

--- discoDuroRoer/src/discoDuroRoer_24/ej_24.java
+++ discoDuroRoer/src/discoDuroRoer_24/ej_24.java
@@ -1,10 +1,45 @@
 package discoDuroRoer_24;
 
+import java.util.Scanner;
+
+/**
+ * Rodolfo Aravena
+ * 
+ * Recorre el String << La lluvia en Sevilla es una maravilla >> y 
+ * transforma cada carácter a su código ASCII. 
+ * Muestralos en linea recta, separados por un espacio entre cada carácter.
+ */
+
 public class ej_24 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String vocales = "La lluvia en Sevilla es una maravilla";
+		
+		char res;
+
+		do {
+			StringBuilder vocalesAscci = new StringBuilder();
+			
+			for( int i=0 ; i<vocales.length() ; i++) {
+				vocalesAscci.append((int) vocales.charAt(i));
+			}
+			
+			System.out.println(vocalesAscci);
+			
+			System.out.print("¿Desea continuar? (S/N): "); res = sc.next().charAt(0);
+		} while (Character.toUpperCase(res) == 'S');
 
 	}
 
 }
+
+/*
+Scanner sc = new Scanner(System.in);
+char res;
+
+do {
+	
+	System.out.print("¿Desea continuar? (S/N): "); res = sc.next().charAt(0);
+} while (Character.toUpperCase(res) == 'S');
+*/
